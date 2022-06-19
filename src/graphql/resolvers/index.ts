ee9eda688bcac55cd47b25072d8735e0e9435d60
Nick Stebb <nickolarse@riseup.net>
2022-06-19 08:12:25 +0000

    copy vf-graphql logic

---
+++ src/graphql/resolvers/index.ts
@@ -0,0 +1,26 @@
+import { ResolverOptions } from '../types.js'
+import { DateTimeResolver as DateTime } from 'graphql-scalars'
+
+import { openConnection } from '../connection.js'
+
+import Query from '../queries'
+// import Mutation from '../mutations'
+
+export default async (options: ResolverOptions) => {
+  const {
+    conductorUri,
+    dnaConfig,
+    traceAppSignals = undefined,
+  } = options
+
+  // prefetch connection for this API schema
+  await openConnection(conductorUri, traceAppSignals)
+
+  return {
+    // scalars
+    DateTime,
+    // root schemas
+    Query: Query(dnaConfig, conductorUri),
+    Mutation: null//Mutation(dnaConfig, conductorUri),
+  }
+}
