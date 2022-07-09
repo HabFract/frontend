3e410da78d6d27d2719f1c82e007cb1924604ac5
Nick Stebb <nickolarse@riseup.net>
2022-07-10 09:17:38 +1200

    create onboarding page

---
+++ src/components/pages/Onboarding.tsx
@@ -0,0 +1,21 @@
+// #region Global Imports
+import React, { useState, useEffect } from 'react'
+// #endregion Global Imports
+
+// #region Local Imports
+import { LoginForm } from '../organisms/LoginForm'
+// import { ListItem } from './styled'
+import { useGetHabitsQuery } from '@/graphql/generated'
+// #endregion Local Imports
+
+interface OnboardingProps {
+  isDark: boolean
+}
+
+export const Onboarding: React.FC<OnboardingProps> = ({ isDark }) => {
+  return typeof localStorage.getItem('username') !== 'string' ? ( // Fetch an agent profile for this user (stubbed)
+    <LoginForm />
+  ) : (
+    <div>Hello, {localStorage.getItem('username')}</div>
+  )
+}
