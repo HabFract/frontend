555a72ba4c8978c15689aac5815269796fb70573
Nick Stebb <nickolarse@riseup.net>
2022-07-14 11:18:19 +1200

    fix pages testing spec

--- src/app/hooks/useMyProfile.tsx
+++ src/app/hooks/useMyProfile.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import { Dispatch, SetStateAction, useContext } from 'react'
 import { Profile } from '@/graphql/generated'
 import { MyProfileContext } from '@/contexts/myProfileContext'
 
 export const useMyProfile = () => {
-  const context = React.useContext(MyProfileContext)
+  const context = useContext(MyProfileContext)
   if (!context) {
     throw new Error(`useMyProfile must be used within a MyProfileProvider`)
   }
-  return context as React.Dispatch<React.SetStateAction<Profile>>[]
+  return context as Dispatch<SetStateAction<Profile>>[]
 }
--- src/components/pages/pages.spec.tsx
+++ src/components/pages/pages.spec.tsx
@@ -1,21 +1,25 @@
+import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { MyProfileProvider } from '@/contexts/myProfileContext'
 import { aProfile } from '@/graphql/generated/mocks'
 
+import { Profile } from '@/graphql/generated'
 import { Home } from './Home'
 
-function renderPageWithUser() {
-  const profile = aProfile()
+function renderPageWithUser(Page: React.FunctionComponent) {
+  const profile: Profile = aProfile()
   return render(
     <MyProfileProvider>
-      <Home />
+      <Page />
     </MyProfileProvider>,
   )
 }
 
 describe('Given a registered user', () => {
-  it('should render a menu fit for a registered user', () => {
-    // const wrapper = mount(<BackCaret />)
-    // expect(wrapper).toMatchSnapshot()
+  describe('When a Home page is rendered', () => {
+    it('should render a menu fit for a registered user', () => {
+      renderPageWithUser(Home)
+      // expect(wrapper).toMatchSnapshot()
+    })
   })
 })
