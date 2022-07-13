6e186178fbb02a73cfd9d18bf1303c42a52e9240
Nick Stebb <nickolarse@riseup.net>
2022-07-14 10:56:19 +1200

    tidy viteconfig

--- vite.config.ts
+++ vite.config.ts
@@ -62,39 +62,6 @@ export default defineConfig({
       less: {
         // 是否启用任意主题色模式，这里不启用
         arbitraryMode: false,
-        // 提供多组变量文件
-        multipleScopeVars: [
-          {
-            // 必需
-            scopeName: 'theme-default',
-            // path 和 varsContent 必选一个
-            path: path.resolve('src/app/theme/light.less'),
-            // varsContent参数等效于 path文件的内容
-            // varsContent:`@primary-color:${defaultPrimaryColor};`
-          },
-          {
-            scopeName: 'theme-dark',
-            path: path.resolve('src/app/theme/dark.less'),
-          },
-          // {
-          //   scopeName: "theme-dark",
-          //   path: [
-          //     path.resolve("src/theme/theme-red-dark.less"),
-          //     path.resolve("src/theme/theme-dark.scss"),
-          //   ],
-          // },
-          // {
-          //   scopeName: "theme-red",
-          //   path: path.resolve("src/theme/theme-red.scss"),
-          // },
-          // {
-          //   scopeName: "theme-red-dark",
-          //   path: [
-          //     path.resolve("src/theme/theme-red-dark.less"),
-          //     path.resolve("src/theme/theme-red-dark.scss"),
-          //   ],
-          // },
-        ],
         // 【注意】 css中不是由主题色变量生成的颜色，也让它抽取到主题css内，可以提高权重
         includeStyleWithColors: [
           {
