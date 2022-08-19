41c296c9532e583f99c5c2e1b220d594774bb06d
Nick Stebb <nickolarse@riseup.net>
2022-08-19 15:25:42 +1200

    correct responsiveness of containers

--- src/app/styled/CentredContentTemplate.ts
+++ src/app/styled/CentredContentTemplate.ts
@@ -8,17 +8,39 @@ export const Container = styled.section.attrs({
 })``
 
 export const Content = styled.main.attrs({
-  className: 'h-full border-1 mx-auto z-20 relative',
+  className: 'z-20 p-0 lg:p-12 md:mx-12 lg:mx-auto relative md:top-12',
 })`
+  height: 100%;
   max-width: 1024px;
+
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  justify-content: center;
+  gap: 10px;
+
+  background: radial-gradient(
+    circle closest-side,
+    rgba(255, 255, 255, 0.05),
+    rgba(8, 186, 129, 0.05)
+  );
+  border-radius: 32px 32px 0px 0px;
 `
 
 export const Illustration = styled.div.attrs({
-  className: 'absolute left-0 bg-tranparent pb-4 pl-32 z-10',
+  className: 'absolute md:left-4 bg-tranparent pb-12 md:pb-4 md:pl-32 z-10',
 })`
+  @media only screen and (max-width: 768px) {
+    width: 300px;
+    left: calc(50% - 150px);
+  }
+  @media only screen and (max-height: 960px) {
+    opacity: 0.25;
+  }
+
   width: 428px;
   height: auto;
-  bottom: 96px;
+  bottom: 16px;
 `
 
 Container.displayName = 'ContentContainer'
